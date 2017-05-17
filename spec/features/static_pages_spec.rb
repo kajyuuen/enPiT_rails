214b58ef108017b8291e13a826d1df1099ae7379
kajyuuen <kajyuuen@gmail.com>
2017-05-18 03:26:30 +0900

    Change routes 5.3.2

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 
 describe 'view home page', type: :feature do
   scenario do
-    visit static_pages_home_url
+    visit root_path
     expect(page).to have_title 'Ruby on Rails Tutorial Sample App'
   end
 end
 
 describe 'view help page', type: :feature do
   scenario do
-    visit static_pages_help_url
+    visit help_path
     expect(page).to have_title 'Help | Ruby on Rails Tutorial Sample App'
   end
 end
 
 describe 'view about page', type: :feature do
   scenario do
-    visit static_pages_about_url
+    visit about_path
     expect(page).to have_title 'About | Ruby on Rails Tutorial Sample App'
   end
 end
