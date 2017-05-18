95d9ee81ee6722c69a40e2e7621690f9dee6211b
kajyuuen <kajyuuen@gmail.com>
2017-05-18 13:32:30 +0900

    Refact rspec

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,22 +1,37 @@
 require 'rails_helper'
 
-describe 'view home page', type: :feature do
-  scenario do
-    visit root_path
-    expect(page).to have_title 'Ruby on Rails Tutorial Sample App'
-  end
-end
+describe 'Static pages' do
 
-describe 'view help page', type: :feature do
-  scenario do
-    visit help_path
-    expect(page).to have_title 'Help | Ruby on Rails Tutorial Sample App'
-  end
-end
+  let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
+  # ブロック({})内の評価結果がit内のshouldのレシーバとなる。
+  subject { page }
 
-describe 'view about page', type: :feature do
-  scenario do
-    visit about_path
-    expect(page).to have_title 'About | Ruby on Rails Tutorial Sample App'
+  describe 'Home page' do
+    before { visit root_path }
+
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('Home |') }
+  end
+
+  describe 'Help page' do
+    before { visit help_path }
+
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
+  end
+
+  describe 'About page' do
+    before { visit about_path }
+
+    it { should have_content('About') }
+    it { should have_title(full_title('About')) }
+  end
+
+  describe 'Contact page' do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
